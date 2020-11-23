5676ac94a9542ffd5042e6d6117dc60ce1d91a33
Oz <osvaldogioia@hotmail.com>
2020-11-23 00:01:55 -0300

    Agregar archivos de proyecto.

---
+++ WebApplication5/Controllers/ClienteController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Core.Objects;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace WebApplication5.Controllers
+{
+    public class ClienteController : ApiController
+    {
+        // GET: Cliente
+        public ObjectResult<Cliente_TraerTodo_Result> Get()
+        {
+            Taller_MecanicoEntities taller_MecanicoEntities = new Taller_MecanicoEntities();
+            var clientes = taller_MecanicoEntities.Cliente_TraerTodo();
+            return clientes;
+        }
+    }
+}
